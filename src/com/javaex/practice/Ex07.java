091e774d190ae3986d82a178deca896e0ada69f0
dhqpqp1234 <dhqpqp1234@naver.com>
2022-04-21 00:03:03 +0900

    2022-04-21 수정

--- src/com/javaex/practice/Ex07.java
+++ src/com/javaex/practice/Ex07.java
@@ -11,8 +11,9 @@ public class Ex07 {
 		
 			for(int i=1; i<=num; i++) {
 				for(int j=1; j<=i; j++) {	
-				} System.out.print(i);
-			}System.out.println();
+					System.out.print(i);
+				} System.out.println();
+			}
 		
 		sc.close();
 	}
